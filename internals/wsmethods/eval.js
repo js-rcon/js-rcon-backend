d733bea3601895aa6a5e8987c8d1dfad7edbcf43
Piero <caf203@gmail.com>
2018-04-20 00:37:45 -0400

    get connected players method

--- internals/wsmethods/eval.js
+++ internals/wsmethods/eval.js
@@ -1,11 +1,11 @@
 module.exports = (RCONConnection, websocket, msg) => {
-  return new Promise((resolve, reject) => {
-    RCONConnection.command(msg.c).then((response) => {
-      websocket.send(JSON.stringify({
-        op: 'EVAL_RESPONSE',
-        c: response,
-        id: msg.id
-      }))
+    return new Promise((resolve, reject) => {
+            RCONConnection.command(msg.c).then((response) => {
+                websocket.send(JSON.stringify({
+                    op: 'EVAL_RESPONSE',
+                    c: response,
+                    id: msg.id
+                }))
+            })
     })
-  })
 }
