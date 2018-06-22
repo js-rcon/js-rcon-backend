ea78f275134622143cf7096a07bcf5f1e9de787c
Curtis <caf203@gmail.com>
2018-06-22 13:01:33 -0400

    return proper message id

--- internals/wsmethods/slap.js
+++ internals/wsmethods/slap.js
@@ -5,13 +5,13 @@ module.exports = (RCONConnection, websocket, msg) => {
       websocket.send(JSON.stringify({
         op: 'SLAP_RESPONSE',
         c: true,
-        id: 21
+        id: msg.id
       }))
     } else {
       websocket.send(JSON.stringify({
         op: 'SLAP_RESPONSE',
         c: false,
-        id: 21
+        id: msg.id
       }))
     }
   }).catch(err => global.rconerror(__filename, err))
